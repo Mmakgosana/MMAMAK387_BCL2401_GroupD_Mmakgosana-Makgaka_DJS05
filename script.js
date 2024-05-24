cb46359f6818a2e666452e527a22357dd92ed2ef
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-24 13:13:27 +0200

    I have declared the reducers

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ const decrement = () => ({ type: DECREMENT });
 const reset = () => ({ type: RESET });
 const add = () => ({ type: ADD });
 const subtract = () => ({ type: SUBTRACT });
+
+// Reducer
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return { ...state, count: state.count + 1 };
+      case DECREMENT:
+        return { ...state, count: state.count - 1 };
+      case RESET:
+        return { ...state, count: 0 };
+      case ADD:
+        return { ...state, count: state.count + 1 };
+      case SUBTRACT:
+        return { ...state, count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
