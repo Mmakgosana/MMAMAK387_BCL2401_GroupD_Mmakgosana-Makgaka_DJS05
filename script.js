ce60cb53a68ee66e9928c275d1e3be0bf6f5b970
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-24 13:10:32 +0200

    I have declared the different functions

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
  const initialState = {
     count: 0
   };
+
+  // Action types
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+const ADD = 'ADD';
+const SUBTRACT = 'SUBTRACT';
