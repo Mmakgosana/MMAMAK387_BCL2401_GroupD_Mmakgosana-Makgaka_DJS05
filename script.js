b4425c7e6499b4a6c8dac4db9615d0afeed21f79
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-24 13:12:04 +0200

    I have declared the action creators I# Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const DECREMENT = 'DECREMENT';
 const RESET = 'RESET';
 const ADD = 'ADD';
 const SUBTRACT = 'SUBTRACT';
+
+// Action creators
+const increment = () => ({ type: INCREMENT });
+const decrement = () => ({ type: DECREMENT });
+const reset = () => ({ type: RESET });
+const add = () => ({ type: ADD });
+const subtract = () => ({ type: SUBTRACT });
