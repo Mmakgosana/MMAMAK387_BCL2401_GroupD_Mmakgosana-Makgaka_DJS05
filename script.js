65a30cef7fd4fe3dfbb6a7c66c1e2e4f5eabcf18
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-24 13:16:32 +0200

    I have created a store

--- script.js
+++ script.js
@@ -34,3 +34,23 @@ const reducer = (state = initialState, action) => {
         return state;
     }
   };
+
+ // Create store
+const createStore = (reducer) => {
+    let state;
+    const listeners = [];
+  
+    const getState = () => state;
+  
+    const dispatch = (action) => {
+      state = reducer(state, action);
+      listeners.forEach(listener => listener());
+    };
+  
+    const subscribe = (listener) => {
+      listeners.push(listener);
+      return () => {
+        const index = listeners.indexOf(listener);
+        listeners.splice(index, 1);
+      };
+    }; 
